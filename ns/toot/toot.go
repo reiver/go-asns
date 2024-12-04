edb25168648f66b9c2223b935513d28bdf566d20
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-12-04 02:59:03 -0800

    changed fields based on what is shown here: https://docs.joinmastodon.org/spec/activitypub/#toot , with some additions from inline ns in activty file

--- ns/toot/toot.go
+++ ns/toot/toot.go
@@ -10,25 +10,17 @@ type Toot struct {
 	NameSpace jsonld.NameSpace `jsonld:"http://joinmastodon.org/ns#"`
 	Prefix    jsonld.Prefix    `jsonld:"toot"`
 
-	Curve25519Key    opt.Optional[string] `jsonld:"Curve25519Key,omitempty"`
-	CipherText       opt.Optional[string] `jsonld:"cipherText,omitempty"`
-	Claim            opt.Optional[string] `jsonld:"claim,omitempty"`
-	Device           opt.Optional[string] `jsonld:"Device,omitempty"`
-	DeviceID         opt.Optional[string] `jsonld:"deviceId,omitempty"`
+
+	AttributionDomains opt.Optional[string] `jsonld:"attributionDomains,omitempty"`
+	BlurHash           opt.Optional[string] `jsonld:"blurhash,omitempty"`
 	Discoverable     opt.Optional[bool]   `jsonld:"discoverable,omitempty"`
-	Ed25519Key       opt.Optional[string] `jsonld:"Ed25519Key,omitempty"`
-	Ed25519Signature opt.Optional[string] `jsonld:"Ed25519Signature,omitempty"`
 	Emoji            opt.Optional[string] `jsonld:"Emoji,omitempty"`
-	EncryptedMessage opt.Optional[string] `jsonld:"EncryptedMessage,omitempty"`
 	Featured         opt.Optional[bool]   `jsonld:"featured,omitempty"`
 	FeaturedTags     []string             `jsonld:"featuredTags,omitempty"`
-	FingerPrintKey   opt.Optional[string] `jsonld:"fingerprintKey,omitempty"`
 	FocalPoint       []any                `jsonld:"focalPoint,omitempty"`
-	IdentityKey      opt.Optional[string] `jsonld:"identityKey,omitempty"`
+	IdentityProof    opt.Optional[string]   `jsonld:"IdentityProof,omitempty"`
 	Indexable        opt.Optional[bool]   `jsonld:"indexable,omitempty"`
 	Memorial         opt.Optional[bool]   `jsonld:"memorial,omitempty"`
-	MessageFranking  opt.Optional[string] `jsonld:"messageFranking,omitempty"`
-	MessageType      opt.Optional[string] `jsonld:"messageType,omitempty"`
-	PublicKeyBase64  opt.Optional[string] `jsonld:"publicKeyBase64,omitempty"`
+	VotersCount        opt.Optional[string] `jsonld:"votersCount,omitempty"`
 	Suspended        opt.Optional[bool]   `jsonld:"suspended,omitempty"`
 }
