da9b668e128b39a6288a92e7f4116f8bf9d476a4
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-12-04 01:54:19 -0800

    act/ns/ap.Actor (ActivityPub actor name-space)

--- ns/ap/actor.go
+++ ns/ap/actor.go
@@ -44,17 +44,53 @@ type Actor struct {
 	EndPoints                  opt.Optional[string] `jsonld:"endpoints,omitempty"`
 	Following                  opt.Optional[string] `jsonld:"following,omitempty"`
 	Followers                  opt.Optional[string] `jsonld:"followers,omitempty"`
+
+	// inbox
+	//
+	// A reference to an ActivityStreams (1) OrderedCollection (2) comprised of all the messages received by the actor; see 5.2 Inbox (3).
+	//
+	// (1) https://www.w3.org/TR/activitystreams-core/
+	//
+	// (2) https://www.w3.org/TR/activitystreams-vocabulary/#dfn-orderedcollection
+	//
+	// (3) https://www.w3.org/TR/activitypub/#inbox
 	Inbox                      opt.Optional[string] `jsonld:"inbox,omitempty"`
+
 	Liked                      opt.Optional[string] `jsonld:"liked,omitempty"`
 	Shares                     opt.Optional[string] `jsonld:"shares,omitempty"`
 	Likes                      opt.Optional[string] `jsonld:"likes,omitempty"`
 	OauthAuthorizationEndPoint opt.Optional[string] `jsonld:"oauthAuthorizationEndpoint,omitempty"`
 	OauthTokenEndPoint         opt.Optional[string] `jsonld:"oauthTokenEndpoint,omitempty"`
+
+	// outbox
+	//
+	// An ActivityStreams (1) OrderedCollection (2) comprised of all the messages produced by the actor; see 5.1 Outbox (3).
+	//
+	// (1) https://www.w3.org/TR/activitystreams-core/
+	//
+	// (2) https://www.w3.org/TR/activitystreams-vocabulary/#dfn-orderedcollection
+	//
+	// (3) https://www.w3.org/TR/activitypub/#outbox
 	Outbox                     opt.Optional[string] `jsonld:"outbox,omitempty"`
+
+	// preferredUsername
+	//
+	// A short username which may be used to refer to the actor, with no uniqueness guarantees.
 	PreferredUserName          opt.Optional[string] `jsonld:"preferredUsername,omitempty"`
+
 	ProvideClientKey           opt.Optional[string] `jsonld:"provideClientKey,omitempty"`
 	ProxyURL                   opt.Optional[string] `jsonld:"proxyUrl,omitempty"`
+
+	// sharedInbox
+	//
+	// An optional endpoint used for wide delivery of publicly addressed activities and activities sent to followers. (1)
+	// sharedInbox endpoints SHOULD also be publicly readable OrderedCollection objects containing objects addressed to the Public (2) special collection. Reading from the sharedInbox endpoint MUST NOT present objects which are not addressed to the Public endpoint.
+	//
+	// (1) https://www.w3.org/TR/activitypub/#shared-inbox-delivery
+	//
+	// (2) https://www.w3.org/TR/activitypub/#public-addressing
 	SharedInbox                opt.Optional[string] `jsonld:"sharedInbox,omitempty"`
+
 	SignClientKey              opt.Optional[string] `jsonld:"signClientKey,omitempty"`
 	Source                     opt.Optional[string] `jsonld:"source,omitempty"`
 	Streams                    opt.Optional[string] `jsonld:"streams,omitempty"`
