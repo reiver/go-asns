aead81c15703889eb816a6f67cf24ddd00681b8b
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-12-04 02:59:33 -0800

    changed fields based on what is shown here: https://docs.joinmastodon.org/spec/activitypub/#toot , with some additions from inline ns in activty file

--- ns/toot/toot.go
+++ ns/toot/toot.go
@@ -13,14 +13,14 @@ type Toot struct {
 
 	AttributionDomains opt.Optional[string] `jsonld:"attributionDomains,omitempty"`
 	BlurHash           opt.Optional[string] `jsonld:"blurhash,omitempty"`
-	Discoverable     opt.Optional[bool]   `jsonld:"discoverable,omitempty"`
-	Emoji            opt.Optional[string] `jsonld:"Emoji,omitempty"`
-	Featured         opt.Optional[bool]   `jsonld:"featured,omitempty"`
-	FeaturedTags     []string             `jsonld:"featuredTags,omitempty"`
-	FocalPoint       []any                `jsonld:"focalPoint,omitempty"`
-	IdentityProof    opt.Optional[string]   `jsonld:"IdentityProof,omitempty"`
-	Indexable        opt.Optional[bool]   `jsonld:"indexable,omitempty"`
-	Memorial         opt.Optional[bool]   `jsonld:"memorial,omitempty"`
+	Discoverable       opt.Optional[bool]   `jsonld:"discoverable,omitempty"`
+	Emoji              opt.Optional[string] `jsonld:"Emoji,omitempty"`
+	Featured           opt.Optional[bool]   `jsonld:"featured,omitempty"`
+	FeaturedTags     []string               `jsonld:"featuredTags,omitempty"`
+	FocalPoint       []any                  `jsonld:"focalPoint,omitempty"`
+	IdentityProof      opt.Optional[string] `jsonld:"IdentityProof,omitempty"`
+	Indexable          opt.Optional[bool]   `jsonld:"indexable,omitempty"`
+	Memorial           opt.Optional[bool]   `jsonld:"memorial,omitempty"`
 	VotersCount        opt.Optional[string] `jsonld:"votersCount,omitempty"`
-	Suspended        opt.Optional[bool]   `jsonld:"suspended,omitempty"`
+	Suspended          opt.Optional[bool]   `jsonld:"suspended,omitempty"`
 }
