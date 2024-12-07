64a94094300858c6c7bb7bacb12b78fe79431477
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-12-07 06:53:04 -0800

    as: icon mediaType

--- ns/as/icon.go
+++ ns/as/icon.go
@@ -5,9 +5,10 @@ import (
 )
 
 type Icon struct {
-	Height opt.Optional[uint64] `json:"height,omitempty"`
-	Name   opt.Optional[string] `json:"name,omitempty"`
-	Type   opt.Optional[string] `json:"type,omitempty"`
-	URL    opt.Optional[string] `json:"url,omitempty"`
-	Width  opt.Optional[uint64] `json:"width,omitempty"`
+	Height    opt.Optional[uint64] `json:"height,omitempty"`
+	MediaType opt.Optional[string] `json:"mediaType,omitempty"`
+	Name      opt.Optional[string] `json:"name,omitempty"`
+	Type      opt.Optional[string] `json:"type,omitempty"`
+	URL       opt.Optional[string] `json:"url,omitempty"`
+	Width     opt.Optional[uint64] `json:"width,omitempty"`
 }
