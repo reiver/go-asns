ea37b926e153da5e7389a0ccec9ffaba35e41381
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-12-08 09:59:23 -0800

    as.Icon

--- ns/as/icon.go
+++ ns/as/icon.go
@@ -1,14 +1,3 @@
 package as
 
-import (
-	"github.com/reiver/go-opt"
-)
-
-type Icon struct {
-	Height    opt.Optional[uint64] `json:"height,omitempty"`
-	MediaType opt.Optional[string] `json:"mediaType,omitempty"`
-	Name      opt.Optional[string] `json:"name,omitempty"`
-	Type      opt.Optional[string] `json:"type,omitempty"`
-	URL       opt.Optional[string] `json:"url,omitempty"`
-	Width     opt.Optional[uint64] `json:"width,omitempty"`
-}
+type Icon = Image
