3371fedb5e2a9f9dadce7564c247d35672cc4ff2
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-12-08 02:24:43 -0800

    as.Generator

---
+++ ns/as/generator.go
@@ -0,0 +1,11 @@
+package as
+
+import (
+	"github.com/reiver/go-opt"
+)
+
+type Generator struct {
+	Name opt.Optional[string] `json:"name,omitempty"`
+	Type opt.Optional[string] `json:"type,omitempty"`
+	URL  opt.Optional[string] `json:"url,omitempty"`
+}
