2f266587cbc9f68c459559ca5c2b70748e4f272f
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-12-08 06:06:20 -0800

    as.ObjectType*

---
+++ ns/as/objecttypes.go
@@ -0,0 +1,16 @@
+package as
+
+const (
+	ObjectTypeArticle      = "Article"      // https://www.w3.org/ns/activitystreams#Article
+	ObjectTypeAudio        = "Audio"        // https://www.w3.org/ns/activitystreams#Audio
+	ObjectTypeDocument     = "Document"     // https://www.w3.org/ns/activitystreams#Document
+	ObjectTypeEvent        = "Event"        // https://www.w3.org/ns/activitystreams#Event
+	ObjectTypeImage        = "Image"        // https://www.w3.org/ns/activitystreams#Image
+	ObjectTypeNote         = "Note"         // https://www.w3.org/ns/activitystreams#Note
+	ObjectTypePage         = "Page"         // https://www.w3.org/ns/activitystreams#Page
+	ObjectTypePlace        = "Place"        // https://www.w3.org/ns/activitystreams#Place
+	ObjectTypeProfile      = "Profile"      // https://www.w3.org/ns/activitystreams#Profile
+	ObjectTypeRelationship = "Relationship" // https://www.w3.org/ns/activitystreams#Relationship
+	ObjectTypeTombstone    = "Tombstone"    // https://www.w3.org/ns/activitystreams#Tombstone
+	ObjectTypeVideo        = "Video"        // https://www.w3.org/ns/activitystreams#Video
+)
