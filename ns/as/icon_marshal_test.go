a5253e65235fdfaff075b97ace34beba3d2c2591
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-12-08 10:16:21 -0800

    as.Icon

--- ns/as/icon_marshal_test.go
+++ ns/as/icon_marshal_test.go
@@ -5,20 +5,20 @@ import (
 
 	"bytes"
 
-	"github.com/reiver/go-jsonld"
 	"github.com/reiver/go-opt"
 
+	"github.com/reiver/go-act"
 	"github.com/reiver/go-act/ns/as"
 )
 
-func TestIcon_MarshalJSONLD(t *testing.T) {
+func TestIcon_marshal(t *testing.T) {
 
 	tests := []struct{
 		Icon as.Icon
 		Expected []byte
 	}{
 		{
-			Expected: []byte(`{}`),
+			Expected: []byte(`{"type":"Image"}`),
 		},
 
 
@@ -27,31 +27,31 @@ func TestIcon_MarshalJSONLD(t *testing.T) {
 			Icon: as.Icon{
 				Height: opt.Something(uint64(123)),
 			},
-			Expected: []byte(`{"height":123}`),
+			Expected: []byte(`{"height":123,"type":"Image"}`),
 		},
 		{
 			Icon: as.Icon{
 				Name: opt.Something("apple banana cherry"),
 			},
-			Expected: []byte(`{"name":"apple banana cherry"}`),
+			Expected: []byte(`{"name":"apple banana cherry","type":"Image"}`),
 		},
 		{
 			Icon: as.Icon{
-				Type: opt.Something("image/png"),
+				MediaType: opt.Something("image/png"),
 			},
-			Expected: []byte(`{"type":"image/png"}`),
+			Expected: []byte(`{"mediaType":"image/png","type":"Image"}`),
 		},
 		{
 			Icon: as.Icon{
 				URL: opt.Something("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg=="),
 			},
-			Expected: []byte(`{"url":"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg=="}`),
+			Expected: []byte(`{"type":"Image","url":"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg=="}`),
 		},
 		{
 			Icon: as.Icon{
 				Width: opt.Something(uint64(4567)),
 			},
-			Expected: []byte(`{"width":4567}`),
+			Expected: []byte(`{"type":"Image","width":4567}`),
 		},
 
 
@@ -59,18 +59,18 @@ func TestIcon_MarshalJSONLD(t *testing.T) {
 		{
 			Icon: as.Icon{
 				Height: opt.Something(uint64(123)),
+				MediaType: opt.Something("image/png"),
 				Name: opt.Something("apple banana cherry"),
-				Type: opt.Something("image/png"),
 				URL: opt.Something("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg=="),
 				Width: opt.Something(uint64(4567)),
 			},
-			Expected: []byte(`{"height":123,"name":"apple banana cherry","type":"image/png","url":"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==","width":4567}`),
+			Expected: []byte(`{"height":123,"mediaType":"image/png","name":"apple banana cherry","type":"Image","url":"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==","width":4567}`),
 		},
 	}
 
 	for testNumber, test := range tests {
 
-		actual, err := jsonld.Marshal(test.Icon)
+		actual, err := act.Marshal(test.Icon)
 		if nil != err {
 			t.Errorf("For test #%d, did not expect an error but actually got one.", testNumber)
 			t.Logf("ERROR: (%T) %s", err, err)
