adc1980789dd265a0ccb005c4a8d6fd2d49778c3
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-12-08 09:25:08 -0800

    as.Image

--- ns/as/image.go
+++ ns/as/image.go
@@ -1,14 +1,20 @@
 package as
 
 import (
+	"github.com/reiver/go-json"
 	"github.com/reiver/go-opt"
 )
 
 type Image struct {
-	Height    opt.Optional[uint64] `json:"height,omitempty"`
-	MediaType opt.Optional[string] `json:"mediaType,omitempty"`
-	Name      opt.Optional[string] `json:"name,omitempty"`
-	Type      opt.Optional[string] `json:"type,omitempty"`
-	URL       opt.Optional[string] `json:"url,omitempty"`
-	Width     opt.Optional[uint64] `json:"width,omitempty"`
+	Audiences            []Audience  `json:"audience,omitempty"`
+	Generator              Generator `json:"generator,omitempty"`
+	Height    opt.Optional[uint64]   `json:"height,omitempty"`
+	MediaType opt.Optional[string]   `json:"mediaType,omitempty"`
+	Location               Location  `json:"location,omitempty"`
+	Name      opt.Optional[string]   `json:"name,omitempty"`
+	Published opt.Optional[string]   `json:"published,omitempty"`
+	Type        json.Const[string]   `json:"type" json.value:"Image"`
+	Updated   opt.Optional[string]   `json:"updated,omitempty"`
+	URL       opt.Optional[string]   `json:"url,omitempty"`
+	Width     opt.Optional[uint64]   `json:"width,omitempty"`
 }
