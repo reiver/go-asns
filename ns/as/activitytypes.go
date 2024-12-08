10264cedae93f237169e610cd03d505ddf2de6a2
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-12-08 02:54:22 -0800

    as.ActivityType*

---
+++ ns/as/activitytypes.go
@@ -0,0 +1,32 @@
+package as
+
+const (
+	ActivityTypeAccept          = "Accept"          // https://www.w3.org/ns/activitystreams#Accept
+	ActivityTypeAdd             = "Add"             // https://www.w3.org/ns/activitystreams#Add
+	ActivityTypeAnnounce        = "Announce"        // https://www.w3.org/ns/activitystreams#Announce
+	ActivityTypeArrive          = "Arrive"          // https://www.w3.org/ns/activitystreams#Arrive
+	ActivityTypeBlock           = "Block"           // https://www.w3.org/ns/activitystreams#Block
+	ActivityTypeCreate          = "Create"          // https://www.w3.org/ns/activitystreams#Create
+	ActivityTypeDelete          = "Delete"          // https://www.w3.org/ns/activitystreams#Delete
+	ActivityTypeDislike         = "Dislike"         // https://www.w3.org/ns/activitystreams#Dislike
+	ActivityTypeFlag            = "Flag"            // https://www.w3.org/ns/activitystreams#Flag
+	ActivityTypeFollow          = "Follow"          // https://www.w3.org/ns/activitystreams#Follow
+	ActivityTypeIgnore          = "Ignore"          // https://www.w3.org/ns/activitystreams#Ignore
+	ActivityTypeInvite          = "Invite"          // https://www.w3.org/ns/activitystreams#Invite
+	ActivityTypeJoin            = "Join"            // https://www.w3.org/ns/activitystreams#Join
+	ActivityTypeLeave           = "Leave"           // https://www.w3.org/ns/activitystreams#Leave
+	ActivityTypeLike            = "Like"            // https://www.w3.org/ns/activitystreams#Like
+	ActivityTypeListen          = "Listen"          // https://www.w3.org/ns/activitystreams#Listen
+	ActivityTypeMove            = "Move"            // https://www.w3.org/ns/activitystreams#Move
+	ActivityTypeOffer           = "Offer"           // https://www.w3.org/ns/activitystreams#Offer
+	ActivityTypeQuestion        = "Question"        // https://www.w3.org/ns/activitystreams#Question
+	ActivityTypeReject          = "Reject"          // https://www.w3.org/ns/activitystreams#Reject
+	ActivityTypeRead            = "Read"            // https://www.w3.org/ns/activitystreams#Read
+	ActivityTypeRemove          = "Remove"          // https://www.w3.org/ns/activitystreams#Remove
+	ActivityTypeTentativeReject = "TentativeReject" // https://www.w3.org/ns/activitystreams#TentativeReject
+	ActivityTypeTentativeAccept = "TentativeAccept" // https://www.w3.org/ns/activitystreams#TentativeAccept
+	ActivityTypeTravel          = "Travel"          // https://www.w3.org/ns/activitystreams#Travel
+	ActivityTypeUndo            = "Undo"            // https://www.w3.org/ns/activitystreams#Undo
+	ActivityTypeUpdate          = "Update"          // https://www.w3.org/ns/activitystreams#Update
+	ActivityTypeView            = "View"            // https://www.w3.org/ns/activitystreams#View
+)
