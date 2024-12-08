670021e4516a83cde13e41956d4d8f86070e7236
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-12-08 05:59:36 -0800

    as.ActorType*

---
+++ ns/as/actortypes.go
@@ -0,0 +1,9 @@
+package as
+
+const (
+	ActorTypeApplication  = "Application"  // https://www.w3.org/ns/activitystreams#Application
+	ActorTypeGroup        = "Group"        // https://www.w3.org/ns/activitystreams#Group
+	ActorTypeOrganization = "Organization" // https://www.w3.org/ns/activitystreams#Organization
+	ActorTypePerson       = "Person"       // https://www.w3.org/ns/activitystreams#Person
+	ActorTypeService      = "Service"      // https://www.w3.org/ns/activitystreams#Service
+)
