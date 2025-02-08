3b23787afdf66e476a1d4ea316fcdb12c7074ce4
Charles Iliya Krempeaux <iamreiver@gmail.com>
2025-02-07 18:20:35 -0800

    act.ServeActivity()

---
+++ httperror.go
@@ -0,0 +1,10 @@
+package act
+
+import (
+	"net/http"
+)
+
+// httpError deals with writing a basic HTTP response error.
+func httpError(responseWriter http.ResponseWriter, code int) {
+	http.Error(responseWriter, http.StatusText(code), code)
+}
---
+++ serveactivity.go
@@ -0,0 +1,54 @@
+package act
+
+import (
+	"crypto/sha256"
+	"encoding/base64"
+	"fmt"
+	"net/http"
+
+	"github.com/reiver/go-etag"
+)
+
+func ServeActivity(responseWriter http.ResponseWriter, request *http.Request, activity []byte) {
+
+	var digest [sha256.Size]byte
+	{
+		digest = sha256.Sum256(activity)
+	}
+
+	var cacheDigest string
+	{
+		cacheDigest = fmt.Sprintf("sha-256=:%s:", base64.StdEncoding.EncodeToString(digest[:]))
+	}
+
+	{
+		var header http.Header = responseWriter.Header()
+		if nil == header {
+			httpError(responseWriter, http.StatusInternalServerError)
+			return
+		}
+
+		header.Add("Access-Control-Allow-Origin", "*")
+		header.Add("Cache-Control", "max-age=907")
+		header.Add("Content-Digest", cacheDigest)
+		header.Add("Content-Type", ContentTypeActivity)
+	}
+
+	var eTag string
+	{
+		var format string = fmt.Sprintf("sha256=0x%%0%dX", sha256.Size*2)
+		eTag = fmt.Sprintf(format, digest[:])
+	}
+
+	{
+		var handled bool = etag.Handle(responseWriter, request, eTag)
+		if handled {
+			return
+		}
+	}
+
+	{
+		responseWriter.WriteHeader(http.StatusOK)
+		responseWriter.Write(activity)
+	}
+}
