bc9fd50e9778555efe2ee0f96d92e863f6095450
Charles Iliya Krempeaux <iamreiver@gmail.com>
2025-02-07 17:43:53 -0800

    refactor

--- attachment.go
+++ attachment.go
@@ -1,11 +1,3 @@
-package as
+package act
 
-import (
-	"github.com/reiver/go-opt"
-)
-
-type Attachment struct {
-	Type  opt.Optional[string] `json:"type,omitempty"`
-	Name  opt.Optional[string] `json:"name,omitempty"`
-	Value opt.Optional[string] `json:"value,omitempty"`
-}
+type Attachment any
